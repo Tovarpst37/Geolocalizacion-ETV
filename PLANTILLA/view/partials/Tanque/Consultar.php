00ba6d523f63bdd80b1a6444109a23a381a00ae1
Kedin-C <davidcue1212@gmail.com>
2026-09-11 22:27:06 -0500

    arregle la estructura del consultar de tanque

--- PLANTILLA/view/partials/Tanque/Consultar.php
+++ PLANTILLA/view/partials/Tanque/Consultar.php
@@ -72,7 +72,70 @@
   }
 
   foreach($array as $ob){
-    echo $ob->getCard($ob->getId());
+  $badgeClass = strtolower($ob->getEstado()) === 'activo' ? 'bg-success' : 'bg-danger';
+
+  $id = $ob -> getId();
+
+     
+
+    ?>
+
+        
+                <div class = "col">
+                
+                    <div class="card p-3 h-100" style="width: 100% !important; min-width: 0 !important; d-flex justify-content-center">
+        <img src="/Geolocalizacion/Geolocalizacion-ETV/PLANTILLA/web/assets/img/<?php echo $ob->getImg();?>"
+            style="width: 15rem; height: 12rem; object-fit: contain;" 
+            class="card-img-top mx-auto d-block" 
+            alt="Tanque">            
+            <div class="card-body">
+
+                <ul class="list-group-item">
+                    <li class="badge  <?php echo $badgeClass ?> rounded-pill">Tanque <?php echo $ob->getEstado();?></li>
+                </ul>
+                        
+                        
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><b>Codigo Tanque </b>:   <?php echo $ob->getNombre(); ?></li>
+                        <li class="list-group-item"><b>Tipo</b>:   <?php echo $ob->getTipo();?></li>
+                        <li class="list-group-item"><b>Zoocriadero</b>:   <?php echo $ob->getZoocriadero();?></li>
+                        <li class="list-group-item"><b>Direccion</b>:  <?php echo $ob->getDireccion();?></li>
+                       
+                    </ul>
+                    <div class="card-body">
+                        <a href="<?php echo getUrl('Tanque','Tanque','getEdit', array('id'=>$ob->getId())); ?>" class="btn btn-primary">
+                            Editar
+                        </a>
+                          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal<?php echo $ob->getId() ?>">
+                            Inhabilitar
+                            </button>
+
+
+                     <div class="modal fade" id="exampleModal<?php echo $id?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Inhabilitar Tanque</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p>¿Estás seguro de Inhabilitar este tanque?</p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                <a href="<?php echo getUrl('Tanque','Tanque','postDelete', array('id'=>$ob->getId())); ?>"class="btn btn-danger" type="button">Inhabilitar</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                            
+                                   
+                    </div>
+                    </div>
+                    
+            </div>
+                <?php 
 
   }
 
