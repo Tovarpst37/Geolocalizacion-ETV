f6029e1a869349fb62c9fff6c9aa3079af5ac6e2
Tovarpst37 <migueltovar3700@gmail.com>
2026-09-13 16:02:09 -0500

    Visata de formularios terminadas, faltan el model controlador y validaciones

--- PLANTILLA/view/partials/FormularioAj/Registrar.php
+++ PLANTILLA/view/partials/FormularioAj/Registrar.php
@@ -9,17 +9,52 @@
                 <form action="<?php echo getUrl("FormularioAj", "FormularioAj", "postInsert") ?>" method="POST">
 
                     <div class="mb-3">
-                        <label for="formulariocod" class="form-label">Codigo</label>
-                        <input type="text" class="form-control" id="formulariomLAj" name="formulariomLAj"
+                        <label for="formulariocod" class="form-label">Codigo del seguimiento</label>
+                        <input type="text" class="form-control" id="codAj" name="codAj"
                             placeholder="Ingrese el codigo del seguimiento" required>
                     </div>
 
 
                     <div class="mb-3">
-                        <label for="formulariocod" class="form-label">Ajustes de nivel</label>
-                        <input type="text" class="form-control" id="formularioz" name="formularioz"
-                            placeholder="Ingrese el codigo del seguimiento" required>
+                        <label for="formulariocod" class="form-label">Numero de documento</label>
+                        <input type="text" class="form-control" id="num" name="num"
+                            placeholder="Ingrese el numero de documento" required>
                     </div>
+
+                    <div class="mb-3">
+                        <label for="fecha_horaLi" class="form-label">Fecha y hora del ajuste</label>
+                        <span class="icono">
+                            <i class="fa-solid fa-calender">
+                            </i>
+                        </span>
+                        <input type="datetime-local" class="form-control" id="fecha_hora" name="fecha_hora" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formulariocod" class="form-label">Nivel de agua adicionado</label>
+                        <input type="text" class="form-control" id="nv" name="nv"
+                            placeholder="Ingrese la adicción proporcionada al tanque" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formulariocod" class="form-label">PH medido</label>
+                        <input type="text" class="form-control" id="ph" name="ph" placeholder="Ingrese el Ph del tanque"
+                            required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formulariocod" class="form-label">Temperatura</label>
+                        <input type="text" class="form-control" id="tem" name="tem" placeholder="Ingrese la temperatura"
+                            required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formulariocod" class="form-label">Observaciones</label>
+                        <input type="text" class="form-control" id="ob" name="ob"
+                            placeholder="Ingrese las observaciones" required>
+                    </div>
+
+
 
                     <div class="d-flex justify-content-end gap-2">
                         <button type="reset" class="btn btn-outline-secondary">Lavado</button>
