755668e6546c460f02e544bccc3c0b934eb8ab06
JeanBurbano <jeancarlosburbanopantoja5678@gmail.com>
2026-09-16 22:38:02 -0500

    solucion de problemas final

--- PLANTILLA/view/partials/sidebarAdmin.php
+++ PLANTILLA/view/partials/sidebarAdmin.php
@@ -17,18 +17,18 @@
     font-weight: 600;
   }
 
-  .sidebar .nav-item > a {
+  .sidebar .nav-item>a {
     color: #ffffff;
   }
 
-  .sidebar .nav-item > a i.fas {
+  .sidebar .nav-item>a i.fas {
     color: var(--sb-light);
     width: 22px;
     text-align: center;
   }
 
-  .sidebar .nav-item > a:hover,
-  .sidebar .nav-item > a:hover i.fas {
+  .sidebar .nav-item>a:hover,
+  .sidebar .nav-item>a:hover i.fas {
     color: #ffffff;
     background-color: var(--sb-accent);
     border-radius: 6px;
@@ -101,8 +101,10 @@
           </a>
           <div class="collapse" id="gestionUsuario">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("Usuario", "Usuario", "getCreate"); ?>"><span class="sub-item">Registrar Usuario</span></a></li>
-              <li><a href="<?php echo getUrl("Usuario", "Usuario", "getUsuario"); ?>"><span class="sub-item">Consultar Usuario</span></a></li>
+              <li><a href="<?php echo getUrl("Usuario", "Usuario", "getCreate"); ?>"><span class="sub-item">Registrar
+                    Usuario</span></a></li>
+              <li><a href="<?php echo getUrl("Usuario", "Usuario", "getUsuario"); ?>"><span class="sub-item">Consultar
+                    Usuario</span></a></li>
             </ul>
           </div>
         </li>
@@ -121,22 +123,26 @@
           </a>
           <div class="collapse" id="zoocriaderos">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("Zoocriadero", "Zoocriadero", "getRegistrar") ?>"><span class="sub-item">Registrar Zoocriadero</span></a></li>
-              <li><a href="<?php echo getUrl("Zoocriadero", "Zoocriadero", "getConsultar") ?>"><span class="sub-item">Consultar Zoocriadero</span></a></li>
+              <li><a href="<?php echo getUrl("Zoocriadero", "Zoocriadero", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Zoocriadero</span></a></li>
+              <li><a href="<?php echo getUrl("Zoocriadero", "Zoocriadero", "getConsultar") ?>"><span
+                    class="sub-item">Consultar Zoocriadero</span></a></li>
             </ul>
           </div>
         </li>
 
         <li class="nav-item">
           <a data-bs-toggle="collapse" href="#tanquesZoocriadero">
-            <i class="fas fa-water"></i>
+            <i class="fas fa-prescription-bottle"></i>
             <p>Tanques</p>
             <span class="caret"></span>
           </a>
           <div class="collapse" id="tanquesZoocriadero">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("Tanque", "Tanque", "getRegistrar") ?>"><span class="sub-item">Registrar tanque</span></a></li>
-              <li><a href="<?php echo getUrl("Tanque", "Tanque", "getConsultar") ?>"><span class="sub-item">Consultar tanque</span></a></li>
+              <li><a href="<?php echo getUrl("Tanque", "Tanque", "getRegistrar") ?>"><span class="sub-item">Registrar
+                    tanque</span></a></li>
+              <li><a href="<?php echo getUrl("Tanque", "Tanque", "getConsultar") ?>"><span class="sub-item">Consultar
+                    tanque</span></a></li>
             </ul>
           </div>
         </li>
@@ -149,8 +155,10 @@
           </a>
           <div class="collapse" id="actividadesZoocriadero">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("ActividadZoocriadero", "ActividadZoocriadero", "getRegistrar") ?>"><span class="sub-item">Registrar actividad</span></a></li>
-              <li><a href="<?php echo getUrl("ActividadZoocriadero", "ActividadZoocriadero", "getConsultar") ?>"><span class="sub-item">Consultar actividad</span></a></li>
+              <li><a href="<?php echo getUrl("ActividadZoocriadero", "ActividadZoocriadero", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar actividad</span></a></li>
+              <li><a href="<?php echo getUrl("ActividadZoocriadero", "ActividadZoocriadero", "getConsultar") ?>"><span
+                    class="sub-item">Consultar actividad</span></a></li>
             </ul>
           </div>
         </li>
@@ -163,8 +171,12 @@
           </a>
           <div class="collapse" id="seguimientoZoocriadero">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getRegistrar") ?>"><span class="sub-item">Registrar seguimiento</span></a></li>
-              <li><a href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getConsultar") ?>"><span class="sub-item">Consultar seguimiento</span></a></li>
+              <li><a
+                  href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar seguimiento</span></a></li>
+              <li><a
+                  href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getConsultar") ?>"><span
+                    class="sub-item">Consultar seguimiento</span></a></li>
             </ul>
           </div>
         </li>
@@ -177,36 +189,71 @@
           </a>
           <div class="collapse" id="fomularioZoocriadero">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("FormularioZ", "FormularioZ", "getRegistrar") ?>"><span class="sub-item">Registrar Alimentación</span></a></li>
-              <li><a href="<?php echo getUrl("FormularioM", "FormularioM", "getRegistrar") ?>"><span class="sub-item">Registrar Peces Muertos y nacidos</span></a></li>
-              <li><a href="<?php echo getUrl("FormularioLi", "FormularioLi", "getRegistrar") ?>"><span class="sub-item">Registrar Limpieza</span></a></li>
-              <li><a href="<?php echo getUrl("FormularioAj", "FormularioAj", "getRegistrar") ?>"><span class="sub-item">Registrar Ajuste de Nivel</span></a></li>
-              <li><a href="<?php echo getUrl("FormularioLa", "FormularioLa", "getRegistrar") ?>"><span class="sub-item">Registrar Lavado</span></a></li>
+              <li><a href="<?php echo getUrl("FormularioZ", "FormularioZ", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Alimentación</span></a></li>
+              <li><a href="<?php echo getUrl("FormularioM", "FormularioM", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Peces Muertos y nacidos</span></a></li>
+              <li><a href="<?php echo getUrl("FormularioLi", "FormularioLi", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Limpieza</span></a></li>
+              <li><a href="<?php echo getUrl("FormularioAj", "FormularioAj", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Ajuste de Nivel</span></a></li>
+              <li><a href="<?php echo getUrl("FormularioLa", "FormularioLa", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar Lavado</span></a></li>
             </ul>
           </div>
         </li>
 
         <li class="nav-item">
-          <a data-bs-toggle="collapse" href="#HistorialZoocriadero">
+          <a data-bs-toggle="collapse" href="#Historial">
             <i class="fas fa-history"></i>
             <p>Historial</p>
             <span class="caret"></span>
           </a>
-          <div class="collapse" id="HistorialZoocriadero">
+          <div class="collapse" id="Historial">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("HistorialZoocriadero", "HistorialZoocriadero", "getRegistrar") ?>"><span class="sub-item">Consultar Historial</span></a></li>
+              <li><a href="<?php echo getUrl("Historial", "Historial", "getConsultar") ?>">
+                  <span class="sub-item"> Consultar Historial</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <!-- Trabajo en terreno -->
+        <li class="nav-section">
+          <span class="sidebar-mini-icon">
+            <i class="fa fa-ellipsis-h"></i>
+          </span>
+          <h4 class="text-section">Trabajo en terreno</h4>
+        </li>
+
+
+        <li class="nav-item">
+          <a data-bs-toggle="collapse" href="#Terreno">
+            <i class="fas fa-layer-group"></i>
+            <p>Terreno</p>
+            <span class="caret"></span>
+          </a>
+
+          <div class="collapse" id="Terreno">
+            <ul class="nav nav-collapse">
+              <li>
+                <a href="<?php echo getUrl("Terreno", "Terreno", "getCreate") ?>">
+                  <span class="sub-item"> Registrar terreno</span>
+                </a>
+              </li>
+              <li>
+                <a href="<?php echo getUrl("Terreno", "Terreno", "getConsultar") ?>">
+                  <span class="sub-item"> Consultar terreno</span>
+                </a>
+              </li>
 
             </ul>
           </div>
         </li>
-        <!-- Trabajo en terreno -->
-        <li class="nav-section">
-          <span class="sidebar-mini-icon"><i class="fa fa-ellipsis-h"></i></span>
-          <h4 class="text-section">Trabajo en terreno</h4>
-        </li>
+
+
 
         <li class="nav-item">
-
           <a data-bs-toggle="collapse" href="#sitiosTerreno">
             <i class="fas fa-layer-group"></i>
             <p>Sitios</p>
@@ -224,87 +271,26 @@
                   <span class="sub-item"> Consultar sitio</span>
                 </a>
               </li>
-          <a data-bs-toggle="collapse" href="#Terreno">
-            <i class="fas fa-map-marked-alt"></i>
-            <p>Terreno</p>
-            <span class="caret"></span>
-          </a>
-          <div class="collapse" id="Terreno">
-            <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("Terreno", "Terreno", "getRegistrar") ?>"><span class="sub-item">Registrar terreno</span></a></li>
-              <li><a href="<?php echo getUrl("Terreno", "Terreno", "getConsultar") ?>"><span class="sub-item">Consultar terreno</span></a></li>
             </ul>
           </div>
         </li>
 
-        <li class="nav-item">
-
-          <a data-bs-toggle="collapse" href="#depositoTerrono">
-            <i class="fas fa-layer-group"></i>
-            <p>Tipo de deposito</p>
-
-          <a data-bs-toggle="collapse" href="#sitiosTerreno">
-            <i class="fas fa-map-marker-alt"></i>
-            <p>Sitios</p>
-
-            <span class="caret"></span>
-          </a>
-          <div class="collapse" id="depositoTerrono">
-            <ul class="nav nav-collapse">
-
-              <li>
-                <a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getRegistrar") ?>">
-                  <span class="sub-item"> Registrar tipo de depósito</span>
-                </a>
-              </li>
-              <li>
-                <a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getConsultar") ?>">
-                  <span class="sub-item"> Consultar tipo de depósito</span>
-                </a>
-              </li>
-
-              <li><a href="<?php echo getUrl("Sitios", "Sitios", "getCreate2") ?>"><span class="sub-item">Registrar sitio</span></a></li>
-              <li><a href="<?php echo getUrl("Sitios", "Sitios", "getConsultar") ?>"><span class="sub-item">Consultar sitio</span></a></li>
-            </ul>
-          </div>
-        </li>
-
-
-
-        <li class="nav-item">
-          <a data-bs-toggle="collapse" href="#Terreno">
-            <i class="fas fa-layer-group"></i>
-            <p>Terreno</p>
 
         <li class="nav-item">
           <a data-bs-toggle="collapse" href="#depositoTerrono">
             <i class="fas fa-boxes"></i>
             <p>Tipo de depósito</p>
-
             <span class="caret"></span>
           </a>
-
-          <div class="collapse" id="Terreno">
+          <div class="collapse" id="depositoTerrono">
             <ul class="nav nav-collapse">
-
-              <li>
-                <a href="<?php echo getUrl("Terreno", "Terreno", "getRegistrar") ?>">
-                  <span class="sub-item"> Registrar terreno</span>
-                </a>
-              </li>
-              <li>
-                <a href="<?php echo getUrl("Terreno", "Terreno", "getConsultar") ?>">
-                  <span class="sub-item"> Consultar terreno</span>
-                </a>
-              </li>
-
-              <li><a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getRegistrar") ?>"><span class="sub-item">Registrar tipo de depósito</span></a></li>
-              <li><a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getConsultar") ?>"><span class="sub-item">Consultar tipo de depósito</span></a></li>
-
+              <li><a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar tipo de depósito</span></a></li>
+              <li><a href="<?php echo getUrl("TipoDeDeposito", "TipoDeDeposito", "getConsultar") ?>"><span
+                    class="sub-item">Consultar tipo de depósito</span></a></li>
             </ul>
           </div>
         </li>
-
 
         <li class="nav-item">
           <a data-bs-toggle="collapse" href="#actividadTerrono">
@@ -314,8 +300,10 @@
           </a>
           <div class="collapse" id="actividadTerrono">
             <ul class="nav nav-collapse">
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Registrar actividad</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Consultar actividad</span><span class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Registrar actividad</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Consultar actividad</span><span
+                    class="badge-pending">Próximo</span></a></li>
             </ul>
           </div>
         </li>
@@ -328,22 +316,23 @@
           </a>
           <div class="collapse" id="seguimientoTerreno">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("SeguimientoTerreno", "SeguimientoTerreno", "getRegistrar") ?>"><span class="sub-item">Registrar seguimiento</span></a></li>
-              <li><a href="<?php echo getUrl("SeguimientoTerreno", "SeguimientoTerreno", "getConsultar") ?>"><span class="sub-item">Consultar seguimiento</span></a></li>
+              <li><a href="<?php echo getUrl("SeguimientoTerreno", "SeguimientoTerreno", "getRegistrar") ?>"><span
+                    class="sub-item">Registrar seguimiento</span></a></li>
+              <li><a href="<?php echo getUrl("SeguimientoTerreno", "SeguimientoTerreno", "getConsultar") ?>"><span
+                    class="sub-item">Consultar seguimiento</span></a></li>
             </ul>
           </div>
         </li>
 
         <li class="nav-item">
           <a data-bs-toggle="collapse" href="#fomularioTerreno">
-            <i class="fas fa-file-signature"></i>
-            <p>Formularios</p>
+            <i class="fas fa-layer-group"></i>
+            <p>Fomularios</p>
             <span class="caret"></span>
           </a>
 
           <div class="collapse" id="fomularioTerreno">
             <ul class="nav nav-collapse">
-
               <li>
                 <a href="<?php echo getUrl("FormularioInsp", "FormularioInsp", "getRegistrar") ?>">
                   <span class="sub-item"> Registrar Inspección</span>
@@ -366,9 +355,6 @@
               </li>
 
 
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Registrar formulario</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Consultar formulario</span><span class="badge-pending">Próximo</span></a></li>
-
             </ul>
           </div>
 
@@ -382,7 +368,8 @@
           </a>
           <div class="collapse" id="HistorialTerreno">
             <ul class="nav nav-collapse">
-              <li><a href="<?php echo getUrl("HistorialTerreno", "HistorialTerreno", "getRegistrar") ?>"><span class="sub-item">Consultar Historial</span></a></li>
+              <li><a href="<?php echo getUrl("HistorialTerreno", "HistorialTerreno", "getRegistrar") ?>"><span
+                    class="sub-item">Consultar Historial</span></a></li>
             </ul>
           </div>
         </li>
@@ -401,14 +388,22 @@
           </a>
           <div class="collapse" id="reportesTerrono">
             <ul class="nav nav-collapse">
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Seguimiento de actividades</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Peces nacidos/muertos por tanque</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Tanques por zoocriadero</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Sitios registrados</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Actividades de terreno por tipo</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Actividades por auxiliar</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><span class="sub-item">Tipo de depósitos</span><span class="badge-pending">Próximo</span></a></li>
-              <li class="sub-item-pending"><a href="#"><i class="fas fa-file-excel"></i><span class="sub-item">Exportar a Excel</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Seguimiento de actividades</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Peces nacidos/muertos por
+                    tanque</span><span class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Tanques por zoocriadero</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Sitios registrados</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Actividades de terreno por
+                    tipo</span><span class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Actividades por auxiliar</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><span class="sub-item">Tipo de depósitos</span><span
+                    class="badge-pending">Próximo</span></a></li>
+              <li class="sub-item-pending"><a href="#"><i class="fas fa-file-excel"></i><span class="sub-item">Exportar
+                    a Excel</span></a></li>
             </ul>
           </div>
         </li>
