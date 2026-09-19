4c553bcdf30043751496326e806e5ae8afcb3f00
anamaria <anamaria@gmail.com>
2026-09-18 20:28:09 -0500

    cambio en la consulta de la actividad del terreno

--- PLANTILLA/model/ActividadTerreno/ActividadTerreno.php
+++ PLANTILLA/model/ActividadTerreno/ActividadTerreno.php
@@ -2,13 +2,15 @@
 
 class ActividadTerreno
 {
-    private int $id;
-    private string $codigo;
-    private string $nombre;
-    private string $estado;
 
-    public function __construct(int $id, string $codigo, string $nombre, string $estado)
+    private int $id;
+    private  String $codigo;
+    private  String $nombre;
+    private  String $estado;
+
+    public function __construct(int $id, String $codigo, String $nombre, String $estado)
     {
+
         $this->id = $id;
         $this->codigo = $codigo;
         $this->nombre = $nombre;
@@ -16,60 +18,59 @@ class ActividadTerreno
     }
 
 
-    public function getCard()
-    {
-        $badgeClass = strtolower($this->estado) === 'activo' ? 'bg-success' : 'bg-danger';
 
 
-?>
-        <div class="col">
+   public function getId()
+{
+    return $this->id;
+}
 
-            <div class="card p-3 h-100" style="width: 100% !important; min-width: 0 !important; d-flex justify-content-center">
+public function getCodigo()
+{
+    return $this->codigo;
+}
 
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><b>codigo</b>: <?php echo $this->codigo; ?></li>
-                    <li class="list-group-item"><b>Nombre</b>: <?php echo $this->nombre; ?></li>
-                    <li class="list-group-item"><b>Estado</b>: <?php echo $this->estado; ?></li>
+public function getNombre()
+{
+    return $this->nombre;
+}
 
-                </ul>
-
-                <div class="card-body">
-                    <a href="<?php echo getUrl('ActividadTerreno', 'ActividadTerreno', 'getEdit', array('id' => $this->id)); ?>" class="btn btn-primary">
-                        Editar
-                    </a>
-                    <a href="<?php echo getUrl("ActividadTerreno", "ActividadTerreno", "getDelete", array("id" => $this->id)) ?>" class="btn btn-danger">
-                        Eliminar
-                    </a>
-
-                </div>
-            </div>
-
-        </div>
-<?php
+public function getEstado()
+{
+    return $this->estado;
+}
 
 
-    }
+
 
     public function setId($id)
     {
+
         $this->id = $id;
     }
 
+
     public function setCodigo($codigo)
     {
+
         $this->codigo = $codigo;
     }
 
+
+
     public function setNombre($nombre)
     {
+
         $this->nombre = $nombre;
     }
 
     public function setEstado($estado)
     {
+
         $this->estado = $estado;
     }
 }
+
 
 
 ?>
