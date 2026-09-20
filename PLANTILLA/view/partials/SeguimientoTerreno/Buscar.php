51291dab046ceaa79a24cf01b7ad48e0267af1c4
Kedin-C <davidcue1212@gmail.com>
2026-09-20 14:58:12 -0500

    arregle la parte de terminar el seguimiento del zoocriadero

--- PLANTILLA/view/partials/SeguimientoTerreno/Buscar.php
+++ PLANTILLA/view/partials/SeguimientoTerreno/Buscar.php
@@ -1,3 +1,4 @@
+
 <div class="page-header">
     <h4 class="fw-bold mb-3">Seguimiento Terreno</h4>
 
@@ -24,12 +25,12 @@
 
     <?php 
     if(!empty($seguimientos)){
-    foreach($seguimientos as $s){ ?>
+       foreach($seguimientos as $s){ ?>
     
         <div class="list-group-item d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-4 mb-3  border rounded">
             
             <div>
-                <h6 class="mb-1"><?php echo $s['cod_seguimiento']; ?> — Sitio <?php echo $s['nombre_sitio']; ?> — Terreno <?php echo $s['cod_terreno']; ?></h6>
+                <h6 class="mb-1"><?php echo $s['cod_seguimiento']; ?> — Sitio <?php echo $s['nombre_sitio']; ?> — Deposito <?php echo $s['cod_terreno']; ?></h6>
                 <small class="text-muted">
                     <?php echo $s['hora_inicio']; ?> - <?php echo $s['hora_fin']; ?> 
                     &nbsp;|&nbsp; Auxiliar: <?php echo $s['primer_nombre']." ".$s['primer_apellido']; ?>
@@ -41,8 +42,15 @@
             </div>
 
             <div class="d-flex align-items-center gap-2">
-                <span class="badge <?php echo $s['id_estado'] == 1 ? 'bg-success' : 'bg-danger'; ?>">
-                    <?php echo $s['id_estado'] == 1 ? 'Activo' : 'Inactivo'; ?>
+<span class="badge <?php echo $s['id_estado'] == 3 ? 'bg-danger' : ($s['id_estado'] == 4 ? 'bg-primary' : ($s['id_estado'] == 5 ? 'bg-success' : '')); ?>">
+                  
+                    <?php    
+                    switch($s['id_estado']){
+                      case 3: echo 'Pendiente'; break;
+                      case 4: echo 'En proceso'; break;
+                      case 5: echo 'Finalizado'; break; 
+                      
+                    } ?>
                 </span>
                 <a href="<?php echo getUrl("SeguimientoTerreno","SeguimientoTerreno","getEditar",array('id'=>$s['id_seguimiento_terreno']))?>" class="btn btn-primary">Editar</a>
                 <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#seg<?php echo $s['id_seguimiento_terreno'] ?>">
