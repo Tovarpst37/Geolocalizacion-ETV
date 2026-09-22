55e539d7e9b6d3f2cd4dd90a9fcb88179c7ec029
Tovarpst37 <migueltovar3700@gmail.com>
2026-09-22 08:50:52 -0500

    Ruta de historial zoocriadero en seguimiento

--- PLANTILLA/view/partials/sidebarAdmin.php
+++ PLANTILLA/view/partials/sidebarAdmin.php
@@ -124,8 +124,7 @@
                             <li><a
                                     href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getRegistrar"); ?>"><span
                                         class="sub-item">Registrar seguimiento</span></a></li>
-                            <li><a
-                                    href="<?php echo getUrl("SeguimientoZoocriadero", "SeguimientoZoocriadero", "getConsultar"); ?>"><span
+                            <li><a href="<?php echo getUrl("Historial", "Historial", "getConsultar"); ?>"><span
                                         class="sub-item">Consultar seguimiento</span></a></li>
                         </ul>
                     </li>
