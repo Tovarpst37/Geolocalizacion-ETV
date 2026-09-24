8cbff4cf813e0722de0554c506fa6e305f2a0e93
Kedin-C <davidcue1212@gmail.com>
2026-09-24 09:01:35 -0500

    agrege el modulo de zoocriadero

--- PLANTILLA/view/partials/sidebarAdmin.php
+++ PLANTILLA/view/partials/sidebarAdmin.php
@@ -242,7 +242,7 @@ function tieneAlgunPermiso($modulo, array $permisos) {
                     <?php if(tieneAlgunPermiso('Seguimientos Terreno', ['VER','CREAR','EDITAR','ELIMINAR'])): ?>
                         <li class="nav-item">
                             <a href="#" class="nav-link" title="Seguimientos">
-                                <i class="bx bx-route"></i>
+                                <i class="bx bx-radar"></i> 
                                 <span class="text">Seguimientos</span>
                                 <i class="bx bx-chevron-down navCaret text"></i>
                             </a>
