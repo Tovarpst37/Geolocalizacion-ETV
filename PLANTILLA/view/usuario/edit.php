9df646376795dde7149d78d619376fa3bad7f8d9
Kedin-C <davidcue1212@gmail.com>
2026-09-24 11:26:25 -0500

    guardando cambios

--- PLANTILLA/view/usuario/edit.php
+++ PLANTILLA/view/usuario/edit.php
@@ -121,7 +121,7 @@
         </div>
         </div>
             <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                     <button type="submit" class="btn btn-primary">Guardar</button>
             </div>
         </form>
